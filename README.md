716da1b2657b5f9d3907ea324e5031513c5e900b
Akhilesh Bhople <abhople@us.ibm.com>
2018-10-25 17:12:09 +0530

    Automate creation of READMEs for containers
    Signed-off-by: Akhilesh Bhople <abhople@us.ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ReadmeAutomation
 Automate creation of READMEs for containers
+
+# Prerequisites
+* Python3 installed and in system path in Windows Os
+* tkinter, requests and selenium packages for python3 installed using pip 
+* [Google Chrome Driver](https://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip) Place the downloaded chromedriver.exe file under ReadmeAutomation/lib/ directory.
+
+```
+pip install selenium
+pip install tkinter
+pip install requests
+```
+
+# How to use ?
+
+Input is given through a file named ```input_list.csv``` in the same folder as the push.py script.
+The file should contain image names on dockerhub and corresponding dockerfile links on github ppc64le/buildscripts repo.
+The script will generate ```input.txt``` containing image names and corresponding folder names from github repo. 
+
+If the script encounters any error during generation of readmefiles, the script will be terminated and list of updated entries will be stored in ```UpdatedImageList.csv```.
+
+The Publication Statement and Community Support is added statically from file named static_content.txt.
+
+**Note :** Images should always be on ibmcom registry and corresponding folders should always be in build-scripts repo on github under ppc64le.
---
+++ lib/ReadmeGeneration.py
@@ -0,0 +1,78 @@
+'''
+This is a python script that just dumps all the data
+passed to it in a README.md file generated in same folder
+'''
+_filename = "README.md"
+def set_file_name(imagename):
+    global _filename
+    _filename = "lib/readmefiles/"+imagename+".md"
+
+
+    
+def clear_file():
+    '''Function that creates a new file or truncates an existing file if any'''
+    with open(_filename,'w') as file:
+        pass
+
+
+def add_supported_tags_tab(dict_tag_links_on_github, remaining_tags):
+    ''' Function that accepts a dictionary of tag names as key and github links as value
+    and a list of tags with no github lniks
+
+    This information is then appended to the previous contents of README.md file
+    '''
+    with open(_filename,'a') as file:
+        file.write("\n # Supported Tags and respective `Dockerfile` links\n")
+
+        for key, value in dict_tag_links_on_github.items():
+            file.write(" * " + key)
+            file.write( "  [ ( " + key + "/Dockerfile ) ]" + "(" + value + ") \n")
+
+        for tag in remaining_tags:
+            file.write(" * " + tag + "\n")
+
+
+def add_disclaimer_tab():
+    ''' Function to append a static disclaimer to previous contents of README.md file'''
+    with open(_filename,'a') as file:
+        with open('static_content.txt') as rd:
+            text = rd.read()
+        file.write("\n"+text)
+    
+def add_source_repository_tab(reponame, repolink):
+    ''' Function that takes source repository name and its github link as parameters
+    and appends the infomration to previous contents of README.md file'''
+    with open(_filename,'a') as file:
+        file.write("\n# Source Repository \n")
+        file.write( "  [ " + reponame + " ]" + "(" + repolink + ") \n")
+
+
+def add_usage_tab( usage_text ):
+    '''Function that takes usage_text and appends it to the README.md file'''
+    if usage_text:
+        with open(_filename, 'a') as file:
+            file.write("\n# Usage Information \n")
+            file.write( usage_text )
+
+def add_maintainer_tab( maintainer_text ):
+    '''Function that takes usage_text and appends it to the README.md file'''
+    if maintainer_text:
+        with open(_filename, 'a') as file:
+            file.write("\n# Author \n")
+            file.write( maintainer_text )
+
+def add_source_tab( source_text ):
+    '''Function that takes usage_text and appends it to the README.md file'''
+    if source_text:
+        with open(_filename, 'a') as file:
+            file.write("\n# Based On \n")
+            file.write( source_text )
+
+            
+def add_license_tag( license_list ) :
+    '''Function that takes license list and appends it to the README.md file'''
+    with open(_filename,'a') as file:
+        file.write("\n# License Information \n")
+        file.write(" This image uses following licenses \n")
+        for each_license in license_list:
+            file.write( " * " + each_license + "\n")

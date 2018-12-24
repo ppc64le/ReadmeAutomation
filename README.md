59773336bd7310ae94c100b1da0112f70a603570
Akhilesh Bhople <abhople@us.ibm.com>
2018-12-24 14:02:34 +0530

    Update code to work with new Dockerhub UI

--- README.md
+++ README.md
@@ -2,20 +2,21 @@
 Automate creation of READMEs for containers
 
 # Prerequisites
-* Python3 installed and in system path in Windows Os
-* tkinter, requests and selenium packages for python3 installed using pip 
+* Python 2/3 installed and in system path in Windows Os
+* tkinter, requests, future and selenium packages for python installed using pip 
 * [Google Chrome Driver](https://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip) Place the downloaded chromedriver.exe file under ReadmeAutomation/lib/ directory.
 
 ```
 pip install selenium
 pip install tkinter
 pip install requests
+pip install future
 ```
 
 # How to use ?
 
 Input is given through a file named ```input_list.csv``` in the same folder as the push.py script.
-The file should contain image names on dockerhub and corresponding dockerfile links on github ppc64le/buildscripts repo.
+The file should contain image names on dockerhub, corresponding dockerfile links on github ppc64le/buildscripts repo and the licenses used by the image.
 The script will generate ```input.txt``` containing image names and corresponding folder names from github repo. 
 
 If the script encounters any error during generation of readmefiles, the script will be terminated and list of updated entries will be stored in ```UpdatedImageList.csv```.
--- script.py
+++ script.py
@@ -6,33 +6,37 @@ import requests
 def main(line, usernameStrGithub , passwordStrGithub):
 
     try:
-        image_name, folder_name = line.strip().split(',')
+        image_name, folder_name, license_list = line.strip().split(',')
 
-        print("Getting image tags..")
+        print("\n Getting image tags..")
         tag_list = ITA.get_image_tags_from_dockerhub(image_name, registry="ibmcom")
 
                 
         if tag_list:
-            print("Getting dockerfile links..")
+            print(" Getting dockerfile links..")
             dict_tag_links_on_github, usage_text, remaining_tags, maintainer, source= ITA.get_tag_links_from_github(tag_list, folder_name, usernameStrGithub, passwordStrGithub)
 
+            '''
             for key , value in dict_tag_links_on_github.items():
                 print(" Key: ", key)
                 print(" Value: ", value)
 
             print(remaining_tags)
+            '''
             for tag in remaining_tags:
                 dict_tag_links_on_github[tag] = "https://github.com/ppc64le/build-scripts/blob/master/"+folder_name+"/Dockerfiles"
 
             remaining_tags = []
             
-            print("Generating readme file..")
+            print(" Generating readme file..")
+
             RG.set_file_name(image_name)
             RG.clear_file()
             RG.add_supported_tags_tab(dict_tag_links_on_github, remaining_tags)
             RG.add_maintainer_tab(maintainer)
             RG.add_source_tab(source)
             RG.add_usage_tab(usage_text)
+            RG.add_license_tag(license_list)
             RG.add_disclaimer_tab()
                     
         else:
